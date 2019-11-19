be34d9434a0a857458e48157f576407467837ee2
zeng qing xiao <2490965685@qq.com>
2019-11-19 20:48:17 +0800

    1.测试提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    // 对象字面量项尾不能有逗号
-    "comma-dangle": [2, "never"],
-    // 可以给参数重新赋值
-    "no-param-reassign": 0,
-    // 允许全局用require
-    "global-require": 0,
-    // 关闭换行风格检测
-    "linkbreak-style": 0,
-    "indent": 'off',
-    // 'vue/scipt-indent': [
-    //   'error',
-    //   2,
-    //   {
-    //     'baseIndent': 1
-    //   }
-    // ],
-    "max-len" : ["error", {code : 300}]
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
-};
+}
