eac0e04d6241226513f52f6bfbf4811e2dc42ec5
zengqingxiao <2490965685@qq.com>
2019-11-20 20:48:42 +0800

    1. 移动端适配代码提交

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,8 @@ module.exports = {
   plugins: {
     autoprefixer: {},
     "postcss-pxtorem": {
-      rootValue: 75,
-      propList: ["*"]
+      rootValue: 75, // 已ip 6为例 全部除以10(我们的事设计图就是750px)
+      propList: ["*"] // 配置全部属性
     }
   }
 };
