29322a15df8263b77e9f0cc71c6f5f927c74ff94
zeng qing xiao <2490965685@qq.com>
2019-11-21 15:59:39 +0800

    1. axios 安装 2.轮播图插件安装，以及拿取数据

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import store from './store/index.js'
+import VueAwesomeSwiper from 'vue-awesome-swiper' // 轮播图
 import 'lib-flexible' // 移动端的适配
 import './assets/css/reset.css' // 页面的样式统一
 import './assets/fonts/iconfont.css' // 字体图标
-
+import 'swiper/dist/css/swiper.css' // 轮播图样式
 Vue.config.productionTip = false
-
+Vue.use(VueAwesomeSwiper /* { default global options } */) // 轮播图
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="header">
-    <div class="personal">我的</div>
-    <div>MIKO</div>
-    <i class="iconfont icon-chaxun"></i>
+<div>
+    <div class="header">
+      <div class="personal">我的</div>
+      <div>MIKO</div>
+      <i class="iconfont icon-chaxun"></i>
   </div>
+  <div class="swiper-content home-item">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(item, index) in newSongData" :key="index">
+        <img :src="`${item.song.album.blurPicUrl}?param=400y400`" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from 'axios'
 export default {
   name: 'home',
-  components: {}
+  components: {},
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      },
+      newSongData: []
+    }
+  },
+  created () {
+    this.getNewSongs()
+  },
+  methods: {
+    // 头部轮播图
+    async getNewSongs () {
+      const { data } = await axios.get('/api/personalized/newsong')
+      if (data.code === 200) {
+        this.newSongData = data.result
+      }
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -39,5 +71,90 @@ export default {
     i {
       font-size: 36px;
     }
+  }
+
+  .home-item {
+    padding: 20px;
+    margin-bottom: 20px;
+    background-color: white;
+
+    .title-wrapper {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      margin-bottom: 40px;
+
+      .title {
+        padding-left: 20px;
+        font-size: 34px;
+        border-left: 6px solid #f2353c;
+        color: #4c4c4c;
+        line-height: 38px;
+      }
+
+      .more {
+        width: 50px;
+        height: 38px;
+        border: 1px solid #ccc;
+        border-radius: 25px;
+        color: #bfbfbf;
+        text-align: center;
+        line-height: 38px;
+      }
+    }
+  }
+
+  .swiper-content {
+    margin-top: 90px;
+
+    .swiper-container {
+      width: 100%;
+      height: 350px;
+      border-radius: 10px;
+
+      img {
+        width: 100%;
+      }
+    }
+  }
+
+  .nav {
+    padding: 20px 30px 0 30px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    li {
+      text-align: center;
+    }
+
+    .icon-container {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+      background-color: red;
+      margin-bottom: 20px;
+      line-height: 120px;
+    }
+
+    .icon {
+      font-size: 70px;
+      color: white;
+    }
+
+    p{
+      font-size: 24px;
+    }
+  }
+
+  .play-list-wrapper{
+    min-height: 650px;
+  }
+
+  .home-container{
+    margin-top: 90px;
+    height: calc(100vh - 200px);
+    overflow: hidden;
   }
 </style>
